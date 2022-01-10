4b1e7af2470bd116d0636279200774d587ff4c4f
pvv <pvv@equifax.local>
2022-01-10 21:20:30 +0300

    add consul

--- cmd/httpd.go
+++ cmd/httpd.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -18,8 +19,15 @@ var httpdCmd = &cobra.Command{
 	Long:  `Start httpd for info functions`,
 	Run: func(cmd *cobra.Command, args []string) {
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			log.Println(`new connection: `, r.UserAgent(), r.RemoteAddr)
-			fmt.Fprintf(w, "Hello World!")
+			resp, err := http.Get(`http://127.0.0.1:8500/ui`)
+			if err != nil {
+				log.Println(err)
+			} else {
+				_, err := io.Copy(w, resp.Body)
+				if err != nil {
+					log.Println(err)
+				}
+			}
 		})
 
 		http.HandleFunc("/env", func(w http.ResponseWriter, r *http.Request) {
